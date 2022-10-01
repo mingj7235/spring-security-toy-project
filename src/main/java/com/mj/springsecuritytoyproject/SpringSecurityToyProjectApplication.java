b8654978b051bff78cf8b637621e815fa748ac45
joshua <joshuara7235@gmail.com>
2022-10-01 22:23:06 +0900

    git init

---
+++ src/main/java/com/mj/springsecuritytoyproject/SpringSecurityToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mj.springsecuritytoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityToyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityToyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mj/springsecuritytoyproject/SpringSecurityToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.springsecuritytoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
