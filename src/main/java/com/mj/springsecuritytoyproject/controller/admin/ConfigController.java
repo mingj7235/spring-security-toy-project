1b12005e508f35f06e7049d4b3046e2279dc0223
joshua <joshuara7235@gmail.com>
2022-10-03 00:04:23 +0900

    feat : config, messages, mypage, register html init
    feat : Controllers for mapping html (config, messages, mypage)

--- src/main/java/com/mj/springsecuritytoyproject/controller/admin/ConfigController.java
+++ src/main/java/com/mj/springsecuritytoyproject/controller/admin/ConfigController.java
@@ -1,8 +1,18 @@
 package com.mj.springsecuritytoyproject.controller.admin;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ConfigController {
+
+    @GetMapping ("/config")
+    public String config() {
+        return "admin/config";
+    }
 
 }
--- src/main/java/com/mj/springsecuritytoyproject/controller/user/UserController.java
+++ src/main/java/com/mj/springsecuritytoyproject/controller/user/UserController.java
@@ -1,8 +1,17 @@
 package com.mj.springsecuritytoyproject.controller.user;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
+    @GetMapping ("mypage")
+    public String myPage() {
+        return "user/mypage";
+    }
 }
