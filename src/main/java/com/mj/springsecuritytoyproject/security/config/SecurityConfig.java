fc0a23b5e9ca4366d43fc55202bdadc1e52d0d7f
joshua <joshuara7235@gmail.com>
2022-10-02 11:15:14 +0900

    feat : Security antMatchers 설정, 가상 계정 설정
    feat : MessageController init
    
    fix : Image 파일 경로 설정

--- src/main/java/com/mj/springsecuritytoyproject/security/config/SecurityConfig.java
+++ src/main/java/com/mj/springsecuritytoyproject/security/config/SecurityConfig.java
@@ -1,26 +1,42 @@
 package com.mj.springsecuritytoyproject.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+
+        String password = passwordEncoder().encode("1111");
+
+        auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .antMatchers("/mypage").hasRole("USER")
+                .anyRequest().authenticated()
 
-        http
+        .and()
                 .formLogin()
         ;
-
-        http
-                .csrf();
     }
-
 }
