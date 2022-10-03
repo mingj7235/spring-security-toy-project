3b04e8e5f19d279d920ba52e81677c09bc151217
joshua <joshuara7235@gmail.com>
2022-10-03 13:08:23 +0900

    lecture 4-7 logout

--- src/main/java/com/mj/springsecuritytoyproject/controller/login/LoginController.java
+++ src/main/java/com/mj/springsecuritytoyproject/controller/login/LoginController.java
@@ -1,8 +1,14 @@
 package com.mj.springsecuritytoyproject.controller.login;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -11,6 +17,17 @@ public class LoginController {
     @GetMapping ("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping ("/logout")
+    public String logout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login";
     }
 
 }
