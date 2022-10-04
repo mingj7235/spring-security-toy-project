4f7fac70b6271dc2c7bf3d014acfda884e5f18ca
joshua <joshuara7235@gmail.com>
2022-10-05 00:18:59 +0900

    lecture 5-7 bugfix (Ajax connection)

--- src/main/java/com/mj/springsecuritytoyproject/security/config/AjaxSecurityConfig.java
+++ src/main/java/com/mj/springsecuritytoyproject/security/config/AjaxSecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
